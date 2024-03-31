eccc1ceb253da43669a81fdd1c7e2587f6e38b5a
szaman <2622838+szamanr@users.noreply.github.com>
2024-03-31 23:41:35 +0200

    implement time-since picker (#1)
    * Initial commit from Create Next App
    
    * add basic packages
    
    * display time since
    
    * implement picking start date
    
    * tidy up
    
    * replace page title
    
    * lift state up to prevent hydration errors

---
+++ app/TimeSince.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { pick } from "lodash";
+import React from "react";
+import { format, formatDuration, intervalToDuration } from "date-fns";
+
+type Props = {
+  dateSince: string | null | undefined;
+};
+
+export const TimeSince: React.FC<Props> = ({ dateSince }: Props) => {
+  if (dateSince === undefined) return null;
+  if (!dateSince) return <p className="text-gray-500">Please provide a date</p>;
+
+  const dateDiff = formatDuration(
+    pick(intervalToDuration({ end: new Date(), start: dateSince }), [
+      "years",
+      "months",
+      "weeks",
+      "days",
+    ]),
+  );
+
+  return (
+    <p>
+      <span>It's been </span>
+      <span>
+        <strong>{dateDiff || "0 days"}</strong>
+      </span>
+      <span> since {format(dateSince, "dd MMM yyyy")}.</span>
+    </p>
+  );
+};
